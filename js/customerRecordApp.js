fa09c80690c94a995e11832867a164d1cc3e94cf
Jared Stander <jaredstander@gmail.com>
2016-04-28 17:23:09 -0400

    JSON storage working.

--- js/customerRecordApp.js
+++ js/customerRecordApp.js
@@ -1,20 +1,58 @@
 angular.module('customerRecordApp', [])
 
   .controller('customerRecordAppController', function($scope){
-    $scope.customers = [{name: 'Starfox', email: 'starfox.com', telephone: '666', street: '123 st', city: 'barktown', state: 'MA', zipcode: '00000'}];
-    console.log($scope.customers);
 
     $scope.createCustomer = function() {
       $scope.customers.push({name: $scope.customerName, email: $scope.customerEmail, telephone: $scope.customerTelephone, street: $scope.customerStreet, city: $scope.customerCity, state: $scope.customerState, zipcode: $scope.customerZipcode});
-      console.log('success');
+      window.localStorage['customerRecordAppStorage'] = angular.toJson($scope.customers);
+      $scope.clearForm();
+      console.log("Customer record pushed into customer array, saved into JSON.");
     };
 
     $scope.deleteCustomer = function() {
-
+      // remove at index
+      console.log("Customer record deleted.");
     };
 
     $scope.updateCustomer = function() {
-
+      // pop out the customer record, butfill the form with it.
+      console.log("Customer record _replaced_. (removed at index, re-saved as a new record.)");
     };
+
+    $scope.loadForm = function(customerHash) {
+      $scope.customerName = customerHash['name'];
+      $scope.customerEmail = customerHash['email'];
+      $scope.customerTelephone = customerHash['telephone'];
+      $scope.customerStreet = customerHash['street'];
+      $scope.customerCity = customerHash['city'];
+      $scope.customerState = customerHash['state'];
+      $scope.customerZipcode = customerHash['zipcode'];
+      console.log("Form data filled based on hash passed from customer array.");
+    }
+
+    $scope.clearForm = function() {
+      $scope.customerName = "";
+      $scope.customerEmail = "";
+      $scope.customerTelephone = "";
+      $scope.customerStreet = "";
+      $scope.customerCity = "";
+      $scope.customerState = "";
+      $scope.customerZipcode = "";
+      console.log("Customer fields have all been cleared.");
+    };
+
+    function init() {
+      console.log("Initializing... Checking for existing data...");
+      // Load existing JSON data if there is any.
+      if(window.localStorage['customerRecordAppStorage'] === undefined) {
+        $scope.customers = [];
+        console.log("No JSON storage found, creating black customer array...");
+      }else{
+        $scope.customers = window.localStorage['customerRecordAppStorage'];
+        console.log("JSON storage found, loading customer array...")
+      }
+    };
+
+    init();
 
   });
